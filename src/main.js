a24f10277abc008ed8425a76d20a7e42c3556462
danadum <danadum.empire@gmail.com>
2024-12-29 22:07:03 +0100

    change page title

--- src/main.js
+++ src/main.js
@@ -4,6 +4,4 @@ import { createApp } from "vue";
 import App from "./App.vue";
 
 const app = createApp(App);
-
-document.title = import.meta.env.VITE_GAME === "e4k" ? "E4K rankings" : "GGE rankings";
 app.mount("#app");
